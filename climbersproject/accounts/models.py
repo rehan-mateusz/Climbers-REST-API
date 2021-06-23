1fcd0056f3a83727c10f5e67d43bd50ddfb9ce42
kulas971 <rehan.mateusz@gmail.com>
2021-06-23 13:02:22 +0200

    remove choices

--- climbersproject/accounts/models.py
+++ climbersproject/accounts/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-from model_utils import Choices
-
 class MyAccountManager(BaseUserManager):
     def create_user(self, email, username, password=None):
         if not email:
@@ -36,13 +34,11 @@ class Account(AbstractUser):
         max_length=64, unique=True)
     username            = models.CharField(verbose_name='username',
         max_length=32, unique=True)
-    display_name        = models.CharField(verbose_name='display_name',
-        max_length=32, unique=True)
     weight              = models.IntegerField(verbose_name='weight', blank=True,
-        validators=[MaxValueValidator(150), MinValueValidator(10)])
-    boulder_grade       = models.IntegerField(choices=boulder_grades, blank=True)
-    lead_grade          = models.IntegerField(choices=climbing_grades, blank=True)
-    toprope_grade       = models.IntegerField(choices=climbing_grades, blank=True)
+        null=True, validators=[MaxValueValidator(150), MinValueValidator(10)])
+    boulder_grade       = models.CharField(blank=True, max_length=8)
+    lead_grade          = models.CharField(blank=True, max_length=8)
+    toprope_grade       = models.CharField(blank=True, max_length=8)
 
 
     is_admin            = models.BooleanField(default=False)
@@ -50,17 +46,17 @@ class Account(AbstractUser):
     is_superuser        = models.BooleanField(default=False)
     is_active           = models.BooleanField(default=True)
 
-    boulder_grades = Choices('4', '4+', '5', '5+', '6a', '6a+', '6b', '6b+',
-        '6c', '6c+', '7a', '7a+', '7b', '7b+', '7c', '7c+', '8a', '8a+', '8b',
-        '8b+', '8c', '8c+', '9a')
-    climbing_grades = Choices('I / 1', 'II / 2', 'II+ / 2+', 'III / 3',
-        'IV / 4', 'V- / 4+', 'V / 5a', 'V+ / 5b', 'VI- / 5c', 'VI / 6a',
-        'VI+ / 6a+', 'VII- / 6b', 'VII 6b+', 'VII+ / 6c', 'VIII- / 6c+',
-        'VIII / 7a', 'VIII+ / 7a+', 'IX- / 7b',
-        'IX / 7c', 'IX+ / 7c+', 'X- / 8a+', 'X / 8b', 'X+ / 8b+')
+    # boulder_grades = Choices('4', '4+', '5', '5+', '6a', '6a+', '6b', '6b+',
+    #     '6c', '6c+', '7a', '7a+', '7b', '7b+', '7c', '7c+', '8a', '8a+', '8b',
+    #     '8b+', '8c', '8c+', '9a')
+    # climbing_grades = Choices('I / 1', 'II / 2', 'II+ / 2+', 'III / 3',
+    #     'IV / 4', 'V- / 4+', 'V / 5a', 'V+ / 5b', 'VI- / 5c', 'VI / 6a',
+    #     'VI+ / 6a+', 'VII- / 6b', 'VII 6b+', 'VII+ / 6c', 'VIII- / 6c+',
+    #     'VIII / 7a', 'VIII+ / 7a+', 'IX- / 7b',
+    #     'IX / 7c', 'IX+ / 7c+', 'X- / 8a+', 'X / 8b', 'X+ / 8b+')
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', ]
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
 
     objects = MyAccountManager()
 
