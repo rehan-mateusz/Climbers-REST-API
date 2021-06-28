32f71a51c693166c20cc9ac6e7d05a3aeec7cdf0
kulas971 <rehan.mateusz@gmail.com>
2021-06-28 15:30:20 +0200

    add permission to update users own account

--- climbersproject/accounts/views.py
+++ climbersproject/accounts/views.py
@@ -3,7 +3,9 @@ from rest_framework.response import Response
 
 from . import serializers
 from . import models
+from . import permissions
 
 class AccountViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.AccountSerializer
     queryset = models.Account.objects.all()
+    permission_classes = (permissions.UpdateOwnAccountPermission, )
