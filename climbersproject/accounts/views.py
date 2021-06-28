1ffe6e9affc2f5a2e4206c997b696cf8fe4bb9c8
kulas971 <rehan.mateusz@gmail.com>
2021-06-28 15:36:48 +0200

    add searchFilter to AccountViewSet

--- climbersproject/accounts/views.py
+++ climbersproject/accounts/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework import filters
+
 
 from . import serializers
 from . import models
@@ -8,4 +10,8 @@ from . import permissions
 class AccountViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.AccountSerializer
     queryset = models.Account.objects.all()
+
     permission_classes = (permissions.UpdateOwnAccountPermission, )
+
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ('username')
