9f40545346452d1babb3c8ecef3f072c00c52692
kulas971 <rehan.mateusz@gmail.com>
2021-07-05 12:40:51 +0200

    added Room and Membership models

--- climbersproject/rooms/models.py
+++ climbersproject/rooms/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+class Room(models.model):
+    name    = models.CharField(verbose_name='room_name',
+        max_length=32)
+    owner   = models.ManyToManyField(User, on_delete=models.DO_NOTHING,
+                                     through='Membership')
+
+    """To do: on_delete => custom function
+    to set the oldest member as a new owner"""
+
+class Membership(models.model):
+    account     = models.ForeignKey(User, on_delete=models.CASCADE)
+    room        = models.ForeignKey(Room, on_delete=models.CASCADE)
+    date_joined = models.DateTimeField(default=datetime.now, blank=True)
