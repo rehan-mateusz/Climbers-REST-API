564e6fdb37e927dbe152b63d794ea8ac1113642d
kulas971 <rehan.mateusz@gmail.com>
2021-07-07 12:23:00 +0200

    added path to rooms.urls

--- climbersproject/climbersproject/urls.py
+++ climbersproject/climbersproject/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls', namespace='accounts'))
+    path('accounts/', include('accounts.urls', namespace='accounts')),
+    path('rooms/', include('rooms.urls', namespace='rooms')),
 ]
