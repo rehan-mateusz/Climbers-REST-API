d26a2133cbcb7b576bb9f39ba1ebb4a91a106dd1
kulas971 <rehan.mateusz@gmail.com>
2021-07-07 12:27:03 +0200

    added RoomViewSet

--- climbersproject/rooms/views.py
+++ climbersproject/rooms/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from . import serializers
+from . import models
+
+class RoomViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.RoomSerializer
+    queryset = models.Room.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+        """adds current user to validated_data"""
