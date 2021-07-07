df09ad2e632446772d6e959baaee22d08eaf59f1
kulas971 <rehan.mateusz@gmail.com>
2021-07-07 17:44:44 +0200

    added MembershipSerializer

--- climbersproject/rooms/serializers.py
+++ climbersproject/rooms/serializers.py
@@ -11,11 +11,24 @@ class RoomSerializer(serializers.ModelSerializer):
         new_room = models.Room.objects.create(
             name = validated_data['name'],
             description = validated_data['description'],
-            date = validated_data['date'],
-            )
+            date = validated_data['date'])
         new_membership = models.Membership.objects.create(
             account = validated_data['user'],
-            room = new_room
-        )
+            room = new_room)
         new_room.members.set([validated_data['user']])
         return new_room
+
+class MembershipSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Membership
+        fields = ['room_id', 'account_id', 'date_joined']
+
+    def create(self, validated_data):
+        try:
+            new_membership = models.Membership.objects.create(
+                account = validated_data['user'],
+                room = models.Room.objects.get(id=validated_data['room_id']))
+        except:
+            raise serializers.ValidationError("User is already a member of this room")
+        return new_membership
