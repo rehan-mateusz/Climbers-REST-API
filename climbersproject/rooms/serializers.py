0b6348ab137abc3e01e55eda568cd9f23fa2e48c
kulas971 <rehan.mateusz@gmail.com>
2021-07-07 12:26:20 +0200

    added RoomSerializer

---
+++ climbersproject/rooms/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+
+from datetime import datetime
+
+from . import models
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Room
+        fields = ['name', 'description', 'date', ]
+
+    def create(self, validated_data):
+        new_room = models.Room.objects.create(
+            name = validated_data['name'],
+            description = validated_data['description'],
+            date = validated_data['date'],
+            )
+        new_membership = models.Membership.objects.create(
+            account = validated_data['user'],
+            room = new_room
+        )
+        new_room.members.set([validated_data['user']])
+        return new_room
