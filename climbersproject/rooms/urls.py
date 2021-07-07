5da5a97e9eb1c79031058cfac8ff15d29d6b7407
kulas971 <rehan.mateusz@gmail.com>
2021-07-07 12:24:29 +0200

    added urls.py and registered RoomViewSet to router

---
+++ climbersproject/rooms/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
+from . import views
+
+app_name = 'rooms'
+
+router=DefaultRouter()
+router.register('rooms', views.RoomViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    ]
